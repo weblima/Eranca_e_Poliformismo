b1477a8b2e7dcdcfc5a6833e118cb919fed1bb42
Rafael Lima <rafael_weblima@hotmail.com>
2020-11-17 16:43:24 -0300

    Implementação da classe Products

--- Products/Entities/Product.cs
+++ Products/Entities/Product.cs
@@ -1,8 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Text;
+using System.Globalization;
 
 namespace Products.Entities {
     class Product {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        Product() { 
+        }
+        public Product(string name, double price) {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual string PriceTag() {
+
+            StringBuilder strProducts = new StringBuilder();
+
+            strProducts.Append(Name);
+            strProducts.Append("$ ");
+            strProducts.AppendLine(Price.ToString("F2", CultureInfo.InvariantCulture));
+
+
+            return strProducts.ToString();
+        }
     }
 }
